d9015b3f731e6e77857c5e785ad8a1b9d3478c02
Anita Situmorang <93483591+Situmorang2109@users.noreply.github.com>
2022-04-02 15:34:50 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302194043
+{
+    internal class DataMahasiswa1302201138
+    {
+
+        public class Personal
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string gender { get; set; }
+            public string age { get; set; }
+            public Address address { get; set; }
+            public Cours[] courses { get; set; }
+        }
+
+        public class Address
+        {
+            public string streetAddress { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+        }
+
+        public class Cours
+        {
+            public string code { get; set; }
+            public string name { get; set; }
+        }
+
+        public void ReadJSON()
+        {
+            string file = "jurnal6_1_1302194043.json";
+            string strFile = File.ReadAllText(file);
+            Personal personal = JsonSerializer.Deserialize<Personal>(strFile);
+
+
+            Console.WriteLine($"Mahasiswa {personal.firstName} {personal.lastName} berumur {personal.age} tahun, memiliki jenis kelamin {personal.gender}");
+
+        }
+    }
+}
